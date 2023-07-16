245eda4fb7e6364e641c532d671bca20b028e3cf
Elvis Guanoluisa <elviserock@gmail.com>
2023-07-16 10:12:11 -0500

    Ejemplo de formulario

--- form.php
+++ form.php
@@ -1,13 +1,32 @@
 <?php
    if (isset($_POST['searchterm'])) {
-      // Esto no se debería hacer, es inseguro
-      // echo $_POST['searchterm'];
       echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES) . "<br>";
       echo htmlspecialchars($_POST['searchterm']);
    }
 ?>
 
 <form action="" method="post">
-   <input type="text" name="searchterm">
+   User name: <input type="text" name="searchterm"><br>
+   Password: <input type="password" name="password"><br>
+   Gender:
+      <input type="radio" name="gender" value="f"> female
+      <input type="radio" name="gender" value="m"> male
+      <input type="radio" name="gender" value="o"> other <br>
+   Favorite color:
+      <select name="">
+         <option value="">Please select</option>
+         <option value="#f00">red</option>
+         <option value="#0f0">green</option>
+         <option value="#00f">blue</option>
+      </select><br>
+   Languages spoken:
+      <select name="languages[]" multiple size="3">
+         <option value="en">English</option>
+         <option value="fr">French</option>
+         <option value="it">Italian</option>
+      </select><br>
+   Comments: <textarea name="comments"></textarea><br>
+   <input type="checkbox" name="tc" value="ok">
+      I accept the T&amp;C <br>
    <input type="submit" value="Search">
 </form>
